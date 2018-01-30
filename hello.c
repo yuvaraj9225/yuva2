74e14c21db9c7e43b1089f041ed8abcef95e278c
yuvaraj9225 <35728139+yuvaraj9225@users.noreply.github.com>
2018-01-30 20:48:11 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+ void main()
+ {
+     int a,b;
+     printf("enter the how many times to print the word hello:");
+     scanf("%d",&a);
+     for(b=1;b<=a;b++)
+      printf("hello \n "); 
+ }
