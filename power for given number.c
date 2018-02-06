fdcb24ebc50e92f71b0017c2d23c07cfef7db4c3
yuvaraj9225 <35728139+yuvaraj9225@users.noreply.github.com>
2018-02-06 16:32:12 +0530

    Update power for given number.c

--- power for given number.c
+++ power for given number.c
@@ -3,15 +3,15 @@
 void main()
 {
     int base, exponent,power=1;
-    int a;
+  
     printf("Enter the base :");
     scanf("%d", &base);
     printf("Enter exponent :");
     scanf("%d", &exponent);
-    for(a=1; a<=exponent; a++)
+    for(int a=1; a<=exponent; a++)
     {
         power = power * base;
     }
 
-    printf("power for given number %d for given exponent ^%d=%d",base,exponent,power);
+    printf("power for %d^%d=%d",base,exponent,power);
 }
