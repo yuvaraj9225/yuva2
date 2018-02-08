65a0c7361d8fe1d8933c4cdc4df5d38faa0e7945
yuvaraj9225 <35728139+yuvaraj9225@users.noreply.github.com>
2018-02-08 16:30:45 +0530

    Create print odd numbers between two limits.c

---
+++ print odd numbers between two limits.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int x,y;
+printf("enter the first interval x:\n");
+scanf("%d",&x);
+printf("enter the second interval y:\n");
+scanf("%d",&y);
+printf("\n the odd numbers:");
+for(int i=x;i<y;i++)
+if(i%2!=0)
+{
+printf(" \t  %d",i);
+}
+}
